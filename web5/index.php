d77e75264002150ccabb8f45908d6330136fd86e
Domzzzzz <90952117+Domzzzzz@users.noreply.github.com>
2021-10-18 09:44:41 +0800

    Update index.php

--- web5/index.php
+++ web5/index.php
@@ -5,3 +5,29 @@
 <h1>Hello</h1>
 <p>You have visited this page <?= $num_visits ?> times</p>
 <p><a href="?reset=true">Reset</a></p>
+
+  session_start();
+  $counter_name = "counter.txt";
+
+  // Check if a text file exists.
+  //If not create one and initialize it to zero.
+  if (!file_exists($counter_name)) {
+    $f = fopen($counter_name, "w");
+    fwrite($f,"0");
+    fclose($f);
+  }
+  // Read the current value of our counter file
+  $f = fopen($counter_name,"r");
+  $counterVal = fread($f, filesize($counter_name));
+  fclose($f);
+
+  // Has visitor been counted in this session?
+  // If not, increase counter value by one
+  if(!isset($_SESSION['hasVisited'])){
+    $_SESSION['hasVisited']="yes";
+    $counterVal++;
+    $f = fopen($counter_name, "w");
+    fwrite($f, $counterVal);
+    fclose($f);
+  }
+
